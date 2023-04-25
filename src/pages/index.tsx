af379c43860d847acd778bfb335981a65c553ca2
Eren Nevin <eren.m.nevin@gmail.com>
2023-04-25 18:51:09 +0300

    First Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,271 @@
+import { useContext } from 'react';
+import styled from 'styled-components';
+import { MetamaskActions, MetaMaskContext } from '../hooks';
+import {
+  connectSnap,
+  getSnap,
+  sendHello,
+  sendNotification,
+  sendNotificationRemote,
+  sendVerifiableData,
+  shouldDisplayReconnectButton,
+} from '../utils';
+import {
+  ConnectButton,
+  InstallFlaskButton,
+  ReconnectButton,
+  SendVerifiableDataButton,
+  Card,
+  SendNotificationButton,
+} from '../components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+  margin-top: 7.6rem;
+  margin-bottom: 7.6rem;
+  ${({ theme }) => theme.mediaQueries.small} {
+    padding-left: 2.4rem;
+    padding-right: 2.4rem;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    width: auto;
+  }
+`;
+
+const Heading = styled.h1`
+  margin-top: 0;
+  margin-bottom: 2.4rem;
+  text-align: center;
+`;
+
+const Span = styled.span`
+  color: ${(props) => props.theme.colors.primary.default};
+`;
+
+const Subtitle = styled.p`
+  font-size: ${({ theme }) => theme.fontSizes.large};
+  font-weight: 500;
+  margin-top: 0;
+  margin-bottom: 0;
+  ${({ theme }) => theme.mediaQueries.small} {
+    font-size: ${({ theme }) => theme.fontSizes.text};
+  }
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  max-width: 64.8rem;
+  width: 100%;
+  height: 100%;
+  margin-top: 1.5rem;
+`;
+
+const Notice = styled.div`
+  background-color: ${({ theme }) => theme.colors.background.alternative};
+  border: 1px solid ${({ theme }) => theme.colors.border.default};
+  color: ${({ theme }) => theme.colors.text.alternative};
+  border-radius: ${({ theme }) => theme.radii.default};
+  padding: 2.4rem;
+  margin-top: 2.4rem;
+  max-width: 60rem;
+  width: 100%;
+
+  & > * {
+    margin: 0;
+  }
+  ${({ theme }) => theme.mediaQueries.small} {
+    margin-top: 1.2rem;
+    padding: 1.6rem;
+  }
+`;
+
+const ErrorMessage = styled.div`
+  background-color: ${({ theme }) => theme.colors.error.muted};
+  border: 1px solid ${({ theme }) => theme.colors.error.default};
+  color: ${({ theme }) => theme.colors.error.alternative};
+  border-radius: ${({ theme }) => theme.radii.default};
+  padding: 2.4rem;
+  margin-bottom: 2.4rem;
+  margin-top: 2.4rem;
+  max-width: 60rem;
+  width: 100%;
+  ${({ theme }) => theme.mediaQueries.small} {
+    padding: 1.6rem;
+    margin-bottom: 1.2rem;
+    margin-top: 1.2rem;
+    max-width: 100%;
+  }
+`;
+
+const Index = () => {
+  const [state, dispatch] = useContext(MetaMaskContext);
+
+  const handleConnectClick = async () => {
+    try {
+      await connectSnap();
+      const installedSnap = await getSnap();
+
+      dispatch({
+        type: MetamaskActions.SetInstalled,
+        payload: installedSnap,
+      });
+    } catch (e) {
+      console.error(e);
+      dispatch({ type: MetamaskActions.SetError, payload: e });
+    }
+  };
+
+  const handleSendVeriableDataClick = async () => {
+    try {
+      await sendVerifiableData();
+    } catch (e) {
+      console.error(e);
+      dispatch({ type: MetamaskActions.SetError, payload: e });
+    }
+  };
+
+  const handleSendNotificationClick = async () => {
+    try {
+      await sendNotification();
+    } catch (e) {
+      console.error(e);
+      dispatch({ type: MetamaskActions.SetError, payload: e });
+    }
+  };
+
+  const handleSendNotificationRemoteClick = async () => {
+    try {
+      await sendNotificationRemote();
+    } catch (e) {
+      console.error(e);
+      dispatch({ type: MetamaskActions.SetError, payload: e });
+    }
+  };
+
+  return (
+    <Container>
+      <Heading>
+        Welcome to <Span>Xtreamly</Span>
+      </Heading>
+      <Subtitle>One account to rull them all</Subtitle>
+      <CardContainer>
+        {state.error && (
+          <ErrorMessage>
+            <b>An error happened:</b> {state.error.message}
+          </ErrorMessage>
+        )}
+        {!state.isFlask && (
+          <Card
+            content={{
+              title: 'Install',
+              description:
+                'You need to install MetaMask Flask in order to use Xtreamly',
+              button: <InstallFlaskButton />,
+            }}
+            fullWidth
+          />
+        )}
+        {!state.installedSnap && (
+          <Card
+            content={{
+              title: 'Connect',
+              description: 'Get started by connecting to Xtreamly snap',
+              button: (
+                <ConnectButton
+                  onClick={handleConnectClick}
+                  disabled={!state.isFlask}
+                />
+              ),
+            }}
+            disabled={!state.isFlask}
+          />
+        )}
+        {shouldDisplayReconnectButton(state.installedSnap) && (
+          <Card
+            content={{
+              title: 'Reconnect',
+              description:
+                'While connected to a local running snap this button will always be displayed in order to update the snap if a change is made.',
+              button: (
+                <ReconnectButton
+                  onClick={handleConnectClick}
+                  disabled={!state.installedSnap}
+                />
+              ),
+            }}
+            disabled={!state.installedSnap}
+          />
+        )}
+        <Card
+          content={{
+            title: 'Send Verifiable Data',
+            description:
+              'In the popup, enter your data to be stored in blockchain',
+            button: (
+              <SendVerifiableDataButton
+                onClick={handleSendVeriableDataClick}
+                disabled={!state.installedSnap}
+              />
+            ),
+          }}
+          disabled={!state.installedSnap}
+          fullWidth={
+            state.isFlask &&
+            Boolean(state.installedSnap) &&
+            !shouldDisplayReconnectButton(state.installedSnap)
+          }
+        />
+        <Card
+          content={{
+            title: 'Send To Canister',
+            description: 'Display a custom message as notification',
+            button: (
+              <SendNotificationButton
+                onClick={handleSendNotificationClick}
+                disabled={!state.installedSnap}
+              />
+            ),
+          }}
+          disabled={!state.installedSnap}
+          fullWidth={
+            state.isFlask &&
+            Boolean(state.installedSnap) &&
+            !shouldDisplayReconnectButton(state.installedSnap)
+          }
+        />
+        <Card
+          content={{
+            title: 'Send To Canister Remote',
+            description: 'Display a custom message as notification',
+            button: (
+              <SendNotificationButton
+                onClick={handleSendNotificationRemoteClick}
+                disabled={!state.installedSnap}
+              />
+            ),
+          }}
+          disabled={!state.installedSnap}
+          fullWidth={
+            state.isFlask &&
+            Boolean(state.installedSnap) &&
+            !shouldDisplayReconnectButton(state.installedSnap)
+          }
+        />
+        <Notice>
+          <p>
+            Note that Xtreamly is currently in alpha. Do not use it with your
+            main account
+          </p>
+        </Notice>
+      </CardContainer>
+    </Container>
+  );
+};
+
+export default Index;
